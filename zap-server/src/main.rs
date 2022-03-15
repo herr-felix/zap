0f12771905eaa93c6fdfde03cd30417076ca350d
Felix Beaulieu <felix.beaulieu@protonmail.com>
2022-03-15 14:56:33 -0400

    Benchmark and new allocator

--- zap-server/src/main.rs
+++ zap-server/src/main.rs
@@ -1,15 +1,17 @@
 mod repl;
 
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
+//#[cfg(not(target_env = "msvc"))]
+//use tikv_jemallocator::Jemalloc;
+#[global_allocator]
+static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
 use tokio::net::TcpListener;
 
 use crate::repl::start_repl;
 
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
+//#[cfg(not(target_env = "msvc"))]
+//#[global_allocator]
+//static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> std::io::Result<()> {
@@ -21,7 +23,7 @@ async fn main() -> std::io::Result<()> {
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            start_repl(socket).await.unwrap();
+            start_repl(socket).await.ok();
         });
     }
 }
--- zap/src/lib.rs
+++ zap/src/lib.rs
@@ -7,12 +7,3 @@ pub mod types;
 pub trait Evaluator {
     fn eval<E: env::Env>(&mut self, env: &mut E);
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
