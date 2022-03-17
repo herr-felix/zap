2d6ee597d7665a0348ede8420a70007884083517
Felix Beaulieu <felix.beaulieu@protonmail.com>
2022-03-16 20:44:42 -0400

    Unit test for closing outer variable in fn

--- zap/src/lib.rs
+++ zap/src/lib.rs
@@ -15,7 +15,7 @@ mod tests {
     use crate::reader::Reader;
     use crate::types::String;
 
-    fn run_exp(mut count: usize, src: &str) -> String {
+    fn run_exp(src: &str) -> String {
         let mut reader = Reader::new();
         reader.tokenize(src);
 
@@ -24,80 +24,87 @@ mod tests {
         let env = SandboxEnv::default();
         let mut session = Evaluator::new(env);
 
-        let mut form = reader.read_form(session.get_env()).unwrap().unwrap();
-        while count > 1 {
-            session.eval(form).unwrap();
-            form = reader.read_form(session.get_env()).unwrap().unwrap();
-            count -= 1;
+        let mut form = reader.read_form(session.get_env()).unwrap();
+        let mut res = session.eval(form.unwrap()).unwrap();
+
+        loop {
+            form = reader.read_form(session.get_env()).unwrap();
+            if form.is_none() {
+                return String::from(res.pr_str(session.get_env()))
+            }
+            res = session.eval(form.unwrap()).unwrap();
         }
 
-        let res = session.eval(form).unwrap();
-        String::from(res.pr_str(session.get_env()))
     }
 
     #[test]
     fn eval_number() {
-        assert_eq!(run_exp(1, "1"), "1");
+        assert_eq!(run_exp("1"), "1");
     }
 
     #[test]
     fn eval_if() {
-        assert_eq!(run_exp(1, "(if 12 false true)"), "false");
+        assert_eq!(run_exp("(if 12 false true)"), "false");
     }
 
     #[test]
     fn eval_quoted_symbol() {
-        assert_eq!(run_exp(1, "'a"), "a");
+        assert_eq!(run_exp("'a"), "a");
     }
 
     #[test]
     fn eval_quoted_list() {
-        assert_eq!(run_exp(1, "'(1 2 3)"), "(1 2 3)");
+        assert_eq!(run_exp("'(1 2 3)"), "(1 2 3)");
     }
 
     #[test]
     fn eval_quasiquote_list() {
-        assert_eq!(run_exp(2, "(define x '(2 3)) `(1 x 4)"), "(1 x 4)");
+        assert_eq!(run_exp("(define x '(2 3)) `(1 x 4)"), "(1 x 4)");
     }
 
     #[test]
     fn eval_unquote_list() {
-        assert_eq!(run_exp(2, "(define D '(2 3)) `(1 ~D 4)"), "(1 (2 3) 4)");
+        assert_eq!(run_exp("(define D '(2 3)) `(1 ~D 4)"), "(1 (2 3) 4)");
     }
 
     #[test]
     fn eval_splice_unquote_list() {
-        assert_eq!(run_exp(1, r#"(define c '(1 "b" "d"))"#), r#"(1 "b" "d")"#);
+        assert_eq!(run_exp(r#"(define c '(1 "b" "d"))"#), r#"(1 "b" "d")"#);
         assert_eq!(
-            run_exp(2, r#"(define c '(1 "b" "d")) `(1 c 3)"#),
+            run_exp(r#"(define c '(1 "b" "d")) `(1 c 3)"#),
             r#"(1 c 3)"#
         );
         assert_eq!(
-            run_exp(2, r#"(define c '(1 "b" "d")) `(1 ~@c 3)"#),
+            run_exp(r#"(define c '(1 "b" "d")) `(1 ~@c 3)"#),
             r#"(1 1 "b" "d" 3)"#
         );
         assert_eq!(
-            run_exp(2, r#"(define c '(1 "b" "d")) `(1 ~@c)"#),
+            run_exp(r#"(define c '(1 "b" "d")) `(1 ~@c)"#),
             r#"(1 1 "b" "d")"#
         );
         assert_eq!(
-            run_exp(2, r#"(define c '(1 "b" "d")) `(~@c 2)"#),
+            run_exp(r#"(define c '(1 "b" "d")) `(~@c 2)"#),
             r#"(1 "b" "d" 2)"#
         );
         assert_eq!(
-            run_exp(2, r#"(define c '(1 "b" "d")) `(~@c ~@c)"#),
+            run_exp(r#"(define c '(1 "b" "d")) `(~@c ~@c)"#),
             r#"(1 "b" "d" 1 "b" "d")"#
         );
-        assert_eq!(run_exp(2, "(define x '(2 3)) `(1 ~@x 4)"), "(1 2 3 4)");
+        assert_eq!(run_exp("(define x '(2 3)) `(1 ~@x 4)"), "(1 2 3 4)");
     }
 
     #[test]
     fn eval_let_quote_list() {
-        assert_eq!(run_exp(1, "(let (x '(2 3)) x)"), "(2 3)");
+        assert_eq!(run_exp("(let (x '(2 3)) x)"), "(2 3)");
     }
 
     #[test]
     fn eval_let_unquote_list() {
-        assert_eq!(run_exp(1, "(let (x 0) `~x)"), "0");
+        assert_eq!(run_exp("(let (x 0) `~x)"), "0");
+    }
+
+    #[test]
+    fn eval_returned_function_with_lexical_scoping() {
+        assert_eq!(run_exp("(define same-maker (fn (x) (fn () x))) (define same-4 (same-maker 4)) (same-4)"), "4");
     }
 }
